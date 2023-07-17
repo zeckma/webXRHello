7f6b59f237d3c06ea98320434bd5e082815742dc
zeckma <67948660+zeckma@users.noreply.github.com>
2023-07-17 21:13:31 +0700

    Update index.js_2

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import * as THREE from "../node_modules/three/build/three.module";
+import * as THREE from "../webXRHello/node_modules/three/build/three.module";
 // global scene values
 var btn, gl, glCanvas, camera, scene, renderer, cube;
 
